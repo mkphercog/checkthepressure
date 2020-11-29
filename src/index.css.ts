15730d94b02b0ccae058cb9ac90c1309c27240f7
Marcin Hercog <mkp.hercog.investments@gmail.com>
2020-11-29 18:11:55 +0100

    create base of app install necessary packages delete unnecessary files

---
+++ src/index.css.ts
@@ -0,0 +1,15 @@
+import styled, { createGlobalStyle } from "styled-components";
+
+export const GlobalStyles = createGlobalStyle`
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+ font-family: Arial, Helvetica, sans-serif;
+} 
+`;
+
+export const AppStyled = styled.div`
+  height: 100vh;
+  width: 100wh;
+`;
---
+++ src/pages/Profiles/Profiles.tsx
@@ -0,0 +1,4 @@
+import React from "react";
+import { Wrapper } from "./Profiles.css";
+
+export const Profiles = () => <Wrapper></Wrapper>;
