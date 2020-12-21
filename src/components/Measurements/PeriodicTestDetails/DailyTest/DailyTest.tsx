727b0ebd3614b4debed44a98a8e1cd022d9b0aa4
Marcin Hercog <mkp.hercog.investments@gmail.com>
2020-12-21 21:38:51 +0100

    [create] reusable component MorningEveningDetails

--- src/components/Measurements/PeriodicTestDetails/DailyTest/DailyTest.tsx
+++ src/components/Measurements/PeriodicTestDetails/DailyTest/DailyTest.tsx
@@ -1,52 +1,15 @@
 import React from "react";
-import { useSelector } from "react-redux";
-import { anonymous } from "../../../../common/constants";
-import { COLORS } from "../../../../styles/variables";
-import { IDailyTest, IGlobalState } from "./../../../../common/interfaces";
-import {
-  Wrapper,
-  FieldsetStyled,
-  MorningEvening,
-  SysDiaPuls,
-  SysAndDiaColored,
-  Btns,
-  Btn,
-} from "./DailyTest.css";
+import { IDailyTest } from "./../../../../common/interfaces";
+import { MorningEveningDetails, timeOfDayType } from "./MorningEveningDetails";
+import { Wrapper } from "./DailyTest.css";
 
-enum sysDiaType {
-  SYS = "SYS",
-  DIA = "DIA",
-}
-
-export const DailyTest: React.FC<Props> = ({
+export const DailyTest: React.FC<IProps> = ({
   userID,
   dailyTest,
   editDailyTest,
   setOmittedDaily,
 }) => {
-  const { id, date, morning, evening } = dailyTest;
-  const userBloodPressureBasedOnAge = useSelector(
-    (state: IGlobalState) =>
-      state.profiles.users.find((user) => userID === user.id)
-        ?.userBloodPressureBasedOnAge
-  );
-  const { MIN, MAX } =
-    userBloodPressureBasedOnAge || anonymous.userBloodPressureBasedOnAge;
-
-  const getSysDiaColor = (value: number, type: sysDiaType) => {
-    if (value === 0) return COLORS.black;
-    else if (
-      (value <= MIN.SYS && type === sysDiaType.SYS) ||
-      (value <= MIN.DIA && type === sysDiaType.DIA)
-    )
-      return COLORS.blue;
-    else if (
-      (value >= MAX.SYS && type === sysDiaType.SYS) ||
-      (value >= MAX.DIA && type === sysDiaType.DIA)
-    )
-      return COLORS.red;
-    return COLORS.darkGreen;
-  };
+  const { id, date } = dailyTest;
 
   return (
     <Wrapper>
@@ -54,130 +17,26 @@ export const DailyTest: React.FC<Props> = ({
         Dzień {id} - (<span>{date}</span>)
       </h4>
 
-      <FieldsetStyled
-        sys={morning.SYS}
-        dia={morning.DIA}
-        pulse={morning.PULSE}
-        omitted={morning.omitted}
-      >
-        <MorningEvening
-          sys={morning.SYS}
-          dia={morning.DIA}
-          pulse={morning.PULSE}
-          omitted={morning.omitted}
-        >
-          <p>{morning.timeOfDay}</p>
-        </MorningEvening>
-        <SysDiaPuls>
-          <p>
-            SYS:{" "}
-            <SysAndDiaColored
-              color={
-                morning.omitted
-                  ? COLORS.gray
-                  : getSysDiaColor(morning.SYS, sysDiaType.SYS)
-              }
-            >
-              {morning.SYS}
-            </SysAndDiaColored>
-          </p>
-          <p>
-            DIA:{" "}
-            <SysAndDiaColored
-              color={
-                morning.omitted
-                  ? COLORS.gray
-                  : getSysDiaColor(morning.DIA, sysDiaType.DIA)
-              }
-            >
-              {morning.DIA}
-            </SysAndDiaColored>
-          </p>
-          <p>
-            PULS: <span>{morning.PULSE}</span>
-          </p>
-        </SysDiaPuls>
-        <Btns>
-          <Btn
-            onClick={() => editDailyTest(id, morning.timeOfDay, date)}
-            disabled={morning.omitted}
-          >
-            Edytuj
-          </Btn>
-          <Btn
-            onClick={() =>
-              setOmittedDaily(id, morning.timeOfDay, !morning.omitted)
-            }
-          >
-            {morning.omitted ? "Przywróć" : "Pomiń"}
-          </Btn>
-        </Btns>
-      </FieldsetStyled>
+      <MorningEveningDetails
+        userID={userID}
+        dailyTest={dailyTest}
+        timeOfDayType={timeOfDayType.morning}
+        editDailyTest={editDailyTest}
+        setOmittedDaily={setOmittedDaily}
+      />
 
-      <FieldsetStyled
-        sys={evening.SYS}
-        dia={evening.DIA}
-        pulse={evening.PULSE}
-        omitted={evening.omitted}
-      >
-        <MorningEvening
-          sys={evening.SYS}
-          dia={evening.DIA}
-          pulse={evening.PULSE}
-          omitted={evening.omitted}
-        >
-          <p>{evening.timeOfDay}</p>
-        </MorningEvening>
-        <SysDiaPuls>
-          <p>
-            SYS:{" "}
-            <SysAndDiaColored
-              color={
-                evening.omitted
-                  ? COLORS.gray
-                  : getSysDiaColor(evening.SYS, sysDiaType.SYS)
-              }
-            >
-              {evening.SYS}
-            </SysAndDiaColored>
-          </p>
-          <p>
-            DIA:{" "}
-            <SysAndDiaColored
-              color={
-                evening.omitted
-                  ? COLORS.gray
-                  : getSysDiaColor(evening.DIA, sysDiaType.DIA)
-              }
-            >
-              {evening.DIA}
-            </SysAndDiaColored>
-          </p>
-          <p>
-            PULS: <span>{evening.PULSE}</span>
-          </p>
-        </SysDiaPuls>
-        <Btns>
-          <Btn
-            onClick={() => editDailyTest(id, evening.timeOfDay, date)}
-            disabled={evening.omitted}
-          >
-            Edytuj
-          </Btn>
-          <Btn
-            onClick={() =>
-              setOmittedDaily(id, evening.timeOfDay, !evening.omitted)
-            }
-          >
-            {evening.omitted ? "Przywróć" : "Pomiń"}
-          </Btn>
-        </Btns>
-      </FieldsetStyled>
+      <MorningEveningDetails
+        userID={userID}
+        dailyTest={dailyTest}
+        timeOfDayType={timeOfDayType.evenign}
+        editDailyTest={editDailyTest}
+        setOmittedDaily={setOmittedDaily}
+      />
     </Wrapper>
   );
 };
 
-interface Props {
+interface IProps {
   userID: number;
   dailyTest: IDailyTest;
   editDailyTest: Function;
---
+++ src/components/Measurements/PeriodicTestDetails/DailyTest/MorningEveningDetails.tsx
@@ -0,0 +1,131 @@
+import React from "react";
+import { useSelector } from "react-redux";
+import { anonymous } from "../../../../common/constants";
+import { IDailyTest, IGlobalState } from "../../../../common/interfaces";
+import { COLORS } from "../../../../styles/variables";
+import {
+  FieldsetStyled,
+  MorningEvening as MorningEveningStyled,
+  SysDiaPuls,
+  SysAndDiaColored,
+  Btns,
+  Btn,
+} from "./DailyTest.css";
+
+enum sysDiaType {
+  SYS = "SYS",
+  DIA = "DIA",
+}
+
+export const MorningEveningDetails: React.FC<IProps> = ({
+  userID,
+  dailyTest,
+  timeOfDayType,
+  editDailyTest,
+  setOmittedDaily,
+}) => {
+  const { id, date } = dailyTest;
+  const userBloodPressureBasedOnAge = useSelector(
+    (state: IGlobalState) =>
+      state.profiles.users.find((user) => userID === user.id)
+        ?.userBloodPressureBasedOnAge
+  );
+  const { MIN, MAX } =
+    userBloodPressureBasedOnAge || anonymous.userBloodPressureBasedOnAge;
+
+  const getSysDiaColor = (value: number, type: sysDiaType) => {
+    if (value === 0) return COLORS.black;
+    else if (
+      (value <= MIN.SYS && type === sysDiaType.SYS) ||
+      (value <= MIN.DIA && type === sysDiaType.DIA)
+    )
+      return COLORS.blue;
+    else if (
+      (value >= MAX.SYS && type === sysDiaType.SYS) ||
+      (value >= MAX.DIA && type === sysDiaType.DIA)
+    )
+      return COLORS.red;
+    return COLORS.darkGreen;
+  };
+
+  return (
+    <FieldsetStyled
+      sys={dailyTest[timeOfDayType].SYS}
+      dia={dailyTest[timeOfDayType].DIA}
+      pulse={dailyTest[timeOfDayType].PULSE}
+      omitted={dailyTest[timeOfDayType].omitted}
+    >
+      <MorningEveningStyled
+        sys={dailyTest[timeOfDayType].SYS}
+        dia={dailyTest[timeOfDayType].DIA}
+        pulse={dailyTest[timeOfDayType].PULSE}
+        omitted={dailyTest[timeOfDayType].omitted}
+      >
+        <p>{dailyTest[timeOfDayType].timeOfDay}</p>
+      </MorningEveningStyled>
+      <SysDiaPuls>
+        <p>
+          SYS:{" "}
+          <SysAndDiaColored
+            color={
+              dailyTest[timeOfDayType].omitted
+                ? COLORS.gray
+                : getSysDiaColor(dailyTest[timeOfDayType].SYS, sysDiaType.SYS)
+            }
+          >
+            {dailyTest[timeOfDayType].SYS}
+          </SysAndDiaColored>
+        </p>
+        <p>
+          DIA:{" "}
+          <SysAndDiaColored
+            color={
+              dailyTest[timeOfDayType].omitted
+                ? COLORS.gray
+                : getSysDiaColor(dailyTest[timeOfDayType].DIA, sysDiaType.DIA)
+            }
+          >
+            {dailyTest[timeOfDayType].DIA}
+          </SysAndDiaColored>
+        </p>
+        <p>
+          PULS: <span>{dailyTest[timeOfDayType].PULSE}</span>
+        </p>
+      </SysDiaPuls>
+      <Btns>
+        <Btn
+          onClick={() =>
+            editDailyTest(id, dailyTest[timeOfDayType].timeOfDay, date)
+          }
+          disabled={dailyTest[timeOfDayType].omitted}
+        >
+          Edytuj
+        </Btn>
+        <Btn
+          onClick={() =>
+            setOmittedDaily(
+              id,
+              dailyTest[timeOfDayType].timeOfDay,
+              !dailyTest[timeOfDayType].omitted
+            )
+          }
+        >
+          {dailyTest[timeOfDayType].omitted ? "Przywróć" : "Pomiń"}
+        </Btn>
+      </Btns>
+    </FieldsetStyled>
+  );
+};
+
+export enum timeOfDayType {
+  morning = "morning",
+  evenign = "evening",
+}
+
+interface IProps {
+  userID: number;
+  dailyTest: IDailyTest;
+  timeOfDayType: timeOfDayType;
+  editDailyTest: Function;
+  setOmittedDaily: Function;
+}
