d330ffa1428636b16b6773257708ecb2fd7a6858
Marcin Hercog <mkp.hercog.investments@gmail.com>
2021-01-07 20:40:22 +0100

    [create] Info page

---
+++ src/components/Info/Info.css.ts
@@ -0,0 +1,60 @@
+import styled from "styled-components";
+import {
+  COLORS,
+  TRANSITION_TIME,
+  CORNER_RADIUS,
+} from "./../../styles/variables";
+import { Fieldset } from "../../styles/mixins/Fieldset";
+
+export const FieldsetStyled = styled(Fieldset)`
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
+  width: 90%;
+  margin: 20px 0;
+  overflow: auto;
+
+  @media (min-width: 768px) {
+    width: 70%;
+  }
+
+  @media (min-width: 1024px) {
+    width: 60%;
+  }
+
+  div {
+    margin: 30px 5px;
+    padding: 10px 20px;
+    background-color: ${COLORS.lightGray};
+    border-radius: ${CORNER_RADIUS};
+
+    p {
+      font-size: 14px;
+      text-align: center;
+
+      @media (min-width: 768px) {
+        font-size: 16px;
+      }
+
+      span {
+        font-weight: bold;
+        color: ${COLORS.blue};
+      }
+
+      a {
+        font-weight: bold;
+        text-decoration: none;
+        color: ${COLORS.darkGreen};
+
+        @media (min-width: 1024px) {
+          transition: ${TRANSITION_TIME};
+
+          :hover {
+            color: ${COLORS.green};
+          }
+        }
+      }
+    }
+  }
+`;
--- src/components/Info/Info.tsx
+++ src/components/Info/Info.tsx
@@ -1,6 +1,40 @@
 import React from "react";
 import { SharedPageWrapper } from "./../SharedPageWrapper/SharedPageWrapper";
+import { Legend } from "../../styles/mixins/Fieldset";
+import { FieldsetStyled } from "./Info.css";
 
 export const Info: React.FC = () => (
-  <SharedPageWrapper>W budowie...</SharedPageWrapper>
+  <SharedPageWrapper>
+    <FieldsetStyled>
+      <Legend>Informacje</Legend>
+      <div>
+        <p>
+          <span>Ważna uwaga!</span> Po usunięciu{" "}
+          <span>historii przeglądania w przeglądarce</span> zapisane dane w
+          aplikacji zostaną utracone, ponieważ strona nie wykorzystuje serwera
+          do przechowywania danych!
+        </p>
+      </div>
+      <div>
+        <p>
+          Strona Badaj Ciśnienie jest tylko zapisem okresowych pomiarów
+          ciśnienia krwi użytkownika. Wszelkie oznaczenia kolorystyczne używane
+          na stronie (min, norma, max - względem wieku) są tylko poglądowe i nie
+          zastąpią <span>konsultacji z lekarzem prowadzącym.</span>
+        </p>
+      </div>
+      <div>
+        <p>
+          Projekt i realizacja:{" "}
+          <a
+            href="https://mkphercog.github.io/mywebsite/#/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Marcin Hercog
+          </a>
+        </p>
+      </div>
+    </FieldsetStyled>
+  </SharedPageWrapper>
 );
